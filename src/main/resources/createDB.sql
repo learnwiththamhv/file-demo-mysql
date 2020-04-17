0b1f883faf245ee2cedfdbbae171a3ed044651a4
learnwiththamhv <learnwiththamhv@gmail.com>
2020-04-17 10:37:53 +0700

    Update Create DB Script

--- src/main/resources/createDB.sql
+++ src/main/resources/createDB.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE  IF NOT EXISTS `file_demo`;
-USE `files`;
+USE `file_demo`;
 --
 -- Table structure for tables `files`
 --
 
 DROP TABLE IF EXISTS `files`;
 
-CREATE TABLE file_demo.files (
+CREATE TABLE files (
 	`id` varchar(255) NOT NULL,
 	`file_name` varchar(255) DEFAULT NULL,
 	`file_type` varchar(255) DEFAULT NULL,
