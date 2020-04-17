706bf30a0c46e637cddf56ce12a0b2cbe048f0f9
learnwiththamhv <learnwiththamhv@gmail.com>
2020-04-17 10:32:55 +0700

    SpringBoot Upload/Download File - Mysql

---
+++ src/main/java/xyz/jguru/filedemo/exception/FileStorageException.java
@@ -0,0 +1,11 @@
+package xyz.jguru.filedemo.exception;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/xyz/jguru/filedemo/repository/DBFileRepository.java
@@ -0,0 +1,10 @@
+package xyz.jguru.filedemo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import xyz.jguru.filedemo.model.DBFile;
+
+@Repository
+public interface DBFileRepository extends JpaRepository<DBFile, String> {
+
+}
